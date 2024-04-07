a74962d90b7794f85d207a0caecc9874163614ea
Tim Ott <63533217+TimDOtt@users.noreply.github.com>
2024-04-07 13:17:07 -0500

    Update playRound function with all options

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const choices = ["rock", "paper", "scissors"];
+const playerSelection = "paper";
+const computerSelection = getComputerChoice();
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * choices.length); 
@@ -6,19 +8,33 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function playGame(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
     playerSelection.toLowerCase();
     if (playerSelection === "rock" && computerSelection === "scissors"){
-        return "You win! Rock beats scissors!";
+        return "You win! Rock beats scissors.";
     }
     else if (playerSelection === "rock" && computerSelection === "paper"){
-        return "You lose! Paper beats rock";
+        return "You lose! Paper beats rock.";
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors"){
+        return "You lose! Scissors beats paper.";
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock"){
+        return "You win! Paper beats rock.";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock"){
+        return "You lose! Rock beats scissors.";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper"){
+        return "You win! Scissors beats paper.";
     }
     else {
         return "It's a tie!!";
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playGame(playerSelection, computerSelection));
+function playGame(x) {
+    
+}
+
+console.log(playRound(playerSelection, computerSelection));
